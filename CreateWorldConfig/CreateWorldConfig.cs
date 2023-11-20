7440d7d3753bef2774073e79021f64f7613f41b4
art0007i <art0007i@gmail.com>
2023-11-20 13:13:05 +0100

    resonite

--- CreateWorldConfig/CreateWorldConfig.cs
+++ CreateWorldConfig/CreateWorldConfig.cs
@@ -1,26 +1,25 @@
-using NeosModLoader;
+using ResoniteModLoader;
 using FrooxEngine;
-using BaseX;
+using Elements.Core;
 
 namespace CreateWorldConfig
 {
-    public class CreateWorldConfig : NeosMod
+    public class CreateWorldConfig : ResoniteMod
     {
         public override string Name => "CreateWorldConfig";
         public override string Author => "art0007i";
-        public override string Version => "1.0.1";
+        public override string Version => "2.0.0";
         public override string Link => "https://github.com/art0007i/CreateWorldConfig/";
         public override void OnEngineInit()
         {
             // no harmony required :)
             //Harmony harmony = new Harmony("me.art0007i.CreateWorldConfig");
-            var getConfig = typeof(World).GetField("Configuration", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
 
             Engine.Current.RunPostInit(() =>
             {
                 DevCreateNewForm.AddAction("Editor", "World Configuration", delegate (Slot s)
                 {
-                    WorkerInspector.Create(s, (Worker)getConfig.GetValue(s.World));
+                    WorkerInspector.Create(s, s.World.Configuration);
                 }); 
                 DevCreateNewForm.AddAction("Editor", "World Permission Controller", delegate (Slot s)
                 {
